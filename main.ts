585c1507543327503f313b10da749116409421d7
codingcodey <76607086+codingcodey@users.noreply.github.com>
2021-01-14 21:52:42 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,100 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    coin.destroy(effects.confetti, 100)
+    info.changeLifeBy(1)
+    music.baDing.play()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    pizza.destroy(effects.smiles, 100)
+    info.changeLifeBy(1)
+    music.powerUp.play()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    ghost.setFlag(SpriteFlag.Invisible, true)
+    info.changeLifeBy(-1)
+    pause(1000)
+    ghost.setPosition(110, 60)
+    ghost.setFlag(SpriteFlag.Invisible, false)
+    doggy.startEffect(effects.fire, 500)
+    music.powerDown.play()
+})
+let coin: Sprite = null
+let ghost: Sprite = null
+let pizza: Sprite = null
+let doggy: Sprite = null
+scene.setBackgroundColor(14)
+doggy = sprites.create(img`
+    . . 4 4 4 . . . . 4 4 4 . . . . 
+    . 4 5 5 5 e . . e 5 5 5 4 . . . 
+    4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
+    4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
+    e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
+    . e e 5 5 5 5 5 5 5 5 e e . . . 
+    . . e 5 f 5 5 5 5 f 5 e . . . . 
+    . . f 5 5 5 4 4 5 5 5 f . . f f 
+    . . f 4 5 5 f f 5 5 6 f . f 5 f 
+    . . . f 6 6 6 6 6 6 4 4 f 5 5 f 
+    . . . f 4 5 5 5 5 5 5 4 4 5 f . 
+    . . . f 5 5 5 5 5 4 5 5 f f . . 
+    . . . f 5 f f f 5 f f 5 f . . . 
+    . . . f f . . f f . . f f . . . 
+    `, SpriteKind.Player)
+doggy.setPosition(16, 60)
+pizza = sprites.create(img`
+    . . . . . . b b b b . . . . . . 
+    . . . . . . b 4 4 4 b . . . . . 
+    . . . . . . b b 4 4 4 b . . . . 
+    . . . . . b 4 b b b 4 4 b . . . 
+    . . . . b d 5 5 5 4 b 4 4 b . . 
+    . . . . b 3 2 3 5 5 4 e 4 4 b . 
+    . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+    . . . b 5 3 2 3 5 5 5 5 e e e e 
+    . . b d 7 5 5 5 3 2 3 5 5 e e e 
+    . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+    . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+    . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+    b d 3 2 d 5 5 5 d d d 4 4 . . . 
+    b 5 5 5 5 d d 4 4 4 4 . . . . . 
+    4 d d d 4 4 4 . . . . . . . . . 
+    4 4 4 4 . . . . . . . . . . . . 
+    `, SpriteKind.Food)
+pizza.setPosition(144, 60)
+ghost = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........ffff..........
+    ........ff1111ff........
+    .......fb111111bf.......
+    .......f11111111f.......
+    ......fd11111111df......
+    ....7.fd11111111df......
+    ...7..fd11111111df......
+    ...7..fd11111111df......
+    ...7..fddd1111dddff.....
+    ...77.fbdbfddfbdbfcf....
+    ...777fcdcf11fcdcfbf....
+    ....77fffbdb1bdffcf.....
+    ....fcb1bcffffff........
+    ....f1c1c1ffffff........
+    ....fdfdfdfffff.........
+    .....f.f.f..............
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.Enemy)
+ghost.setPosition(110, 60)
+coin = sprites.create(img`
+    . . b b b b . . 
+    . b 5 5 5 5 b . 
+    b 5 d 3 3 d 5 b 
+    b 5 3 5 5 1 5 b 
+    c 5 3 5 5 1 d c 
+    c d d 1 1 d d c 
+    . f d d d d f . 
+    . . f f f f . . 
+    `, SpriteKind.Projectile)
+info.setLife(3)
+controller.moveSprite(doggy)
+ghost.follow(doggy, 30)
