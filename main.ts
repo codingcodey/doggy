8af447153f86b8e3b0490cb8417c8eb9a1e50d7d
codingcodey <76607086+codingcodey@users.noreply.github.com>
2021-01-19 22:35:32 +1100

    Add coin and add ghost 2.

--- main.ts
+++ main.ts
@@ -1,3 +1,37 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    coin = sprites.create(img`
+        . . b b b b . . 
+        . b 5 5 5 5 b . 
+        b 5 d 3 3 d 5 b 
+        b 5 3 5 5 1 5 b 
+        c 5 3 5 5 1 d c 
+        c d d 1 1 d d c 
+        . f d d d d f . 
+        . . f f f f . . 
+        `, SpriteKind.Projectile)
+    coin.setPosition(randint(0, 160), randint(0, 120))
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    pizza = sprites.create(img`
+        . . . . . . b b b b . . . . . . 
+        . . . . . . b 4 4 4 b . . . . . 
+        . . . . . . b b 4 4 4 b . . . . 
+        . . . . . b 4 b b b 4 4 b . . . 
+        . . . . b d 5 5 5 4 b 4 4 b . . 
+        . . . . b 3 2 3 5 5 4 e 4 4 b . 
+        . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+        . . . b 5 3 2 3 5 5 5 5 e e e e 
+        . . b d 7 5 5 5 3 2 3 5 5 e e e 
+        . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+        . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+        . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+        b d 3 2 d 5 5 5 d d d 4 4 . . . 
+        b 5 5 5 5 d d 4 4 4 4 . . . . . 
+        4 d d d 4 4 4 . . . . . . . . . 
+        4 4 4 4 . . . . . . . . . . . . 
+        `, SpriteKind.Food)
+    pizza.setPosition(randint(0, 160), randint(0, 120))
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
     coin.destroy(effects.confetti, 100)
     info.changeLifeBy(1)
@@ -9,15 +43,24 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSpr
     music.powerUp.play()
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    ghost.setFlag(SpriteFlag.Invisible, true)
-    info.changeLifeBy(-1)
-    pause(1000)
-    ghost.setPosition(110, 60)
-    ghost.setFlag(SpriteFlag.Invisible, false)
-    doggy.startEffect(effects.fire, 500)
-    music.powerDown.play()
+    if (doggy.overlapsWith(ghost)) {
+        ghost.setFlag(SpriteFlag.Invisible, true)
+        info.changeLifeBy(-1)
+        pause(1000)
+        ghost.setFlag(SpriteFlag.Invisible, false)
+        doggy.startEffect(effects.fire, 500)
+        music.powerDown.play()
+    } else {
+        ghost2.setFlag(SpriteFlag.Invisible, true)
+        info.changeLifeBy(-1)
+        pause(1000)
+        ghost2.setFlag(SpriteFlag.Invisible, false)
+        doggy.startEffect(effects.halo, 500)
+        music.powerDown.play()
+    }
 })
 let coin: Sprite = null
+let ghost2: Sprite = null
 let ghost: Sprite = null
 let pizza: Sprite = null
 let doggy: Sprite = null
@@ -57,7 +100,7 @@ pizza = sprites.create(img`
     4 d d d 4 4 4 . . . . . . . . . 
     4 4 4 4 . . . . . . . . . . . . 
     `, SpriteKind.Food)
-pizza.setPosition(144, 60)
+pizza.setPosition(randint(0, 160), randint(0, 120))
 ghost = sprites.create(img`
     ........................
     ........................
@@ -84,7 +127,35 @@ ghost = sprites.create(img`
     ........................
     ........................
     `, SpriteKind.Enemy)
-ghost.setPosition(110, 60)
+ghost.setPosition(112, 90)
+ghost2 = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........ffff..........
+    ........ff1111ff........
+    .......fb111111bf.......
+    .......f11111111f.......
+    ......fd11111111df......
+    ......fd11111111df......
+    ......fddd1111dddf......
+    ......fbdbfddfbdbf......
+    ......fcdcf11fcdcf......
+    .......fb111111bf.......
+    ......fffcdb1bdffff.....
+    ....fc111cbfbfc111cf....
+    ....f1b1b1ffff1b1b1f....
+    ....fbfbffffffbfbfbf....
+    .........ffffff.........
+    ...........fff..........
+    ........................
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.Enemy)
+ghost2.setPosition(120, 21)
+ghost2.follow(doggy, 10)
 coin = sprites.create(img`
     . . b b b b . . 
     . b 5 5 5 5 b . 
