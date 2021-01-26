2695cedb7f6dbed3cb16021ef39489aafb26f14f
codingcodey <76607086+codingcodey@users.noreply.github.com>
2021-01-26 21:47:31 +1100

    Change ghost (their are two) speed and add timer.

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    coin.destroy(effects.confetti, 100)
+    info.changeScoreBy(1)
+    music.baDing.play()
     coin = sprites.create(img`
         . . b b b b . . 
         . b 5 5 5 5 b . 
@@ -11,7 +14,10 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
         `, SpriteKind.Projectile)
     coin.setPosition(randint(0, 160), randint(0, 120))
 })
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    pizza.destroy(effects.smiles, 100)
+    info.changeScoreBy(3)
+    music.powerUp.play()
     pizza = sprites.create(img`
         . . . . . . b b b b . . . . . . 
         . . . . . . b 4 4 4 b . . . . . 
@@ -32,30 +38,20 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         `, SpriteKind.Food)
     pizza.setPosition(randint(0, 160), randint(0, 120))
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
-    coin.destroy(effects.confetti, 100)
-    info.changeLifeBy(1)
-    music.baDing.play()
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
-    pizza.destroy(effects.smiles, 100)
-    info.changeLifeBy(1)
-    music.powerUp.play()
-})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    if (doggy.overlapsWith(ghost)) {
+    if (warrior.overlapsWith(ghost)) {
         ghost.setFlag(SpriteFlag.Invisible, true)
         info.changeLifeBy(-1)
         pause(1000)
         ghost.setFlag(SpriteFlag.Invisible, false)
-        doggy.startEffect(effects.fire, 500)
+        warrior.startEffect(effects.fire, 500)
         music.powerDown.play()
     } else {
         ghost2.setFlag(SpriteFlag.Invisible, true)
         info.changeLifeBy(-1)
         pause(1000)
         ghost2.setFlag(SpriteFlag.Invisible, false)
-        doggy.startEffect(effects.halo, 500)
+        warrior.startEffect(effects.halo, 500)
         music.powerDown.play()
     }
 })
@@ -63,25 +59,27 @@ let coin: Sprite = null
 let ghost2: Sprite = null
 let ghost: Sprite = null
 let pizza: Sprite = null
-let doggy: Sprite = null
+let warrior: Sprite = null
 scene.setBackgroundColor(14)
-doggy = sprites.create(img`
-    . . 4 4 4 . . . . 4 4 4 . . . . 
-    . 4 5 5 5 e . . e 5 5 5 4 . . . 
-    4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
-    4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
-    e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
-    . e e 5 5 5 5 5 5 5 5 e e . . . 
-    . . e 5 f 5 5 5 5 f 5 e . . . . 
-    . . f 5 5 5 4 4 5 5 5 f . . f f 
-    . . f 4 5 5 f f 5 5 6 f . f 5 f 
-    . . . f 6 6 6 6 6 6 4 4 f 5 5 f 
-    . . . f 4 5 5 5 5 5 5 4 4 5 f . 
-    . . . f 5 5 5 5 5 4 5 5 f f . . 
-    . . . f 5 f f f 5 f f 5 f . . . 
-    . . . f f . . f f . . f f . . . 
+warrior = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
-doggy.setPosition(16, 60)
+warrior.setPosition(16, 60)
 pizza = sprites.create(img`
     . . . . . . b b b b . . . . . . 
     . . . . . . b 4 4 4 b . . . . . 
@@ -155,7 +153,7 @@ ghost2 = sprites.create(img`
     ........................
     `, SpriteKind.Enemy)
 ghost2.setPosition(120, 21)
-ghost2.follow(doggy, 10)
+ghost2.follow(warrior, 10)
 coin = sprites.create(img`
     . . b b b b . . 
     . b 5 5 5 5 b . 
@@ -168,5 +166,7 @@ coin = sprites.create(img`
     `, SpriteKind.Projectile)
 coin.setPosition(randint(0, 160), randint(0, 120))
 info.setLife(3)
-controller.moveSprite(doggy)
-ghost.follow(doggy, 30)
+info.setScore(0)
+info.startCountdown(60)
+controller.moveSprite(warrior)
+ghost.follow(warrior, 20)
